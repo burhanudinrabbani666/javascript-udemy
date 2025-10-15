e6eb6e8c47f3f55db6c530572b72bffa859c066a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-15 14:49:24 +0700

    variable

---
+++ javascript-fundamental/index.js
@@ -0,0 +1,13 @@
+/* first lecture : variable, string camelCase
+// variable name country
+let firstName = `burhanudin rabbani`; //let is variable or this call declaring variable
+let country = "🇮🇩 Indonesia";
+let continent = "Asia";
+let population = " 200 million";
+// console.log(firstName);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+*/
+
+//boolean
