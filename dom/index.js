43ea3d70fc05b5c8396f14d942bcf2b249b42e69
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-29 21:39:36 +0700

    implementing dom

--- dom/index.js
+++ dom/index.js
@@ -10,6 +10,8 @@ document.querySelector(`.score`).textContent = 69;
 document.querySelector(`.guess`).value = 66;
 console.log(document.querySelector(`.guess`).value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(`.number`).textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener('click', function () {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -17,6 +19,8 @@ document.querySelector(`.check`).addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector(`.message`).textContent = `No Number ❗`;
+  } else if (guess == secretNumber) {
+    document.querySelector(`.message`).textContent = `Your Correct 🥳`;
   }
 });
 
