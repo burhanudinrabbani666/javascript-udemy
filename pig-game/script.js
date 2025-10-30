6abfa7c0debd3eaf8908b0a4e6aa4bc5bd16104e
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-30 11:18:07 +0700

    active player, toogle, and reset score

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Variables to selecting elements
+const player0Element = document.querySelector(`.player--0`);
+const player1Element = document.querySelector(`.player--1`);
 const score0Element = document.getElementById(`score--0`);
 const score1Elment = document.getElementById(`score--1`);
 const current0Element = document.getElementById(`current--0`);
@@ -16,8 +18,9 @@ score0Element.textContent = 0;
 score1Elment.textContent = 0;
 diceElement.classList.add(`hidden`);
 
+const scores = [0, 0];
 let currentScore = 0;
-
+let activePlayer = 0;
 //addEvent listener
 
 //roll
@@ -28,12 +31,19 @@ btnRollDice.addEventListener(`click`, function () {
   // 2.display dice
   diceElement.classList.remove(`hidden`);
   diceElement.src = `dice-${dice}.png`;
+
   // 3.check for rolled 1: if true, switch to next player
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
-    current0Element.textContent = currentScore; //TODO chage later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle(`player--active`);
+    player1Element.classList.toggle(`player--active`);
   }
 });
