c265b394c93406eed5637574cfcdec101b9e73fc
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-30 09:03:38 +0700

    addEventListener

--- modal/script.js
+++ modal/script.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const btnsOpenModalElement = document.querySelectorAll(`.show-modal`);
-const closeModalElement = document.querySelector(`.close-modal`);
+const btnCloseModalElement = document.querySelector(`.close-modal`);
 const modalElement = document.querySelector(`.modal`);
 const overlayElement = document.querySelector(`.overlay`);
 
-for (let index = 0; index < btnsOpenModalElement.length; index++) {
-  console.log(btnsOpenModalElement[index].textContent);
+// function
+function closeModal() {
+  modalElement.classList.add(`hidden`);
+  overlayElement.classList.add(`hidden`);
 }
+
+function openModal() {
+  modalElement.classList.remove(`hidden`);
+  overlayElement.classList.remove(`hidden`);
+}
+
+//event listeners
+
+for (let index = 0; index < btnsOpenModalElement.length; index++) {
+  btnsOpenModalElement[index].addEventListener(`click`, openModal);
+}
+
+btnCloseModalElement.addEventListener(`click`, closeModal);
+overlayElement.addEventListener(`click`, closeModal);
