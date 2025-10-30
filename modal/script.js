4ec4263bf8f1e8e15b9a8485a176560f1b6b180a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-30 09:28:46 +0700

    addEvenetListener .keydown

--- modal/script.js
+++ modal/script.js
@@ -1,9 +1,12 @@
 'use strict';
-
+//variabel
 const btnsOpenModalElement = document.querySelectorAll(`.show-modal`);
 const btnCloseModalElement = document.querySelector(`.close-modal`);
 const modalElement = document.querySelector(`.modal`);
 const overlayElement = document.querySelector(`.overlay`);
+
+//-------------------------------
+//-------------------------------
 
 // function
 function closeModal() {
@@ -16,6 +19,9 @@ function openModal() {
   overlayElement.classList.remove(`hidden`);
 }
 
+//-------------------------------
+//-------------------------------
+
 //event listeners
 
 for (let index = 0; index < btnsOpenModalElement.length; index++) {
@@ -24,3 +30,13 @@ for (let index = 0; index < btnsOpenModalElement.length; index++) {
 
 btnCloseModalElement.addEventListener(`click`, closeModal);
 overlayElement.addEventListener(`click`, closeModal);
+
+document.addEventListener(`keydown`, function (keyEvent) {
+  console.log(keyEvent.key);
+  if (keyEvent.key === `Escape` && !modalElement.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
+
+//-------------------------------
+//-------------------------------
