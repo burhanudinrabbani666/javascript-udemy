ced787e5fcfaa9e6c7fa0675d18bad89ef9a1f49
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-05 21:08:49 +0700

    feat: maps fundamentall

--- array/assignment/index.js
+++ array/assignment/index.js
@@ -475,3 +475,24 @@ const uniqueKeywordArr = [...uniqueKeyword];
 uniqueKeyword.clear();
 console.log(uniqueKeyword);
 */
+
+//13.1
+const bookMap = new Map([
+  ['title', 'Clean Code'],
+  ['author', 'Robert C. Martin'],
+]);
+
+//13.2
+bookMap.set(`pages`, 464);
+
+//13.3
+console.log(`${bookMap.get(`title`)} by ${bookMap.get(`author`)}`);
+
+//13.4
+console.log(bookMap.size);
+
+//13.5
+
+bookMap.has(`author`)
+  ? console.log(`The author of the book is known`)
+  : `nothing`;
--- array/index.js
+++ array/index.js
@@ -48,8 +48,7 @@ const bettaShop = {
   },
 };
 
-/**ude
-//
+/*
 // destructuring object
 bettaShop.orderDelivery({
   address: 'Jl. Merpati no 23',
@@ -311,7 +310,7 @@ for (const [key, { open, close }] of entries) {
   console.log(`On ${key} we are on at ${open} and close at ${close} `);
 }
 
-*/
+
 
 // SETS
 
@@ -334,3 +333,96 @@ console.log(orderSet);
 for (const order of orderSet) {
   console.log(order);
 }
+*/
+
+// Map
+
+const rest = new Map();
+rest.set(`name`, `classico Italino`);
+rest.set(1, `firanze, Italy`);
+console.log(rest.set(2, `Lisbon, Prrotugal`));
+
+rest
+  .set(`categories`, ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set(`open`, 11)
+  .set(`close`, 23)
+  .set(true, `we are open`)
+  .set(false, `We are closed`);
+
+console.log(rest.get(`name`));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get(`open`) && time < rest.get(`close`)));
+
+console.log(rest.has(`categories`));
+rest.delete(2);
+const arr = [1, 2];
+
+rest.set(document.querySelector(`h1`), `heading`);
+rest.set(arr, `test`);
+console.log(rest);
+
+console.log(rest.get(arr));
+/*
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+
+// Intersection methode
+const commonFoods = italianFoods.intersection(mexicanFoods);
+
+//union methode
+const italianMexicoUnion = italianFoods.union(mexicanFoods);
+
+//difference method
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+const uniqueMexicanFood = mexicanFoods.difference(italianFoods);
+
+// symmetricDifference
+const uniqueItalinAndMexicanFood =
+  italianFoods.symmetricDifference(mexicanFoods);
+*/
